9cb456c87903e97cb37ca6a5a3bcbd11e5044840
2961445 <2961445@stocktonusd.org>
2025-11-12 17:57:31 +0000

    finished the assignemnt

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+'''
+This is the first programming assignment showing user input
+Date: 11/12/25
+'''
+
+#this is a single line comment
+
+name = input("What is your name?")
+
+print ("Hello, " + name)
+
 
